aecd7cad8bb1c5ccdd7acb841d606513c7eb2367
bhargavyagnik <41851612+bhargavyagnik@users.noreply.github.com>
2020-06-05 01:15:36 +0530

    updated

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask,render_template
 import csvdata
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     sarima_cin, pyhat_cin, pyl_cin, pyu_cin= csvdata.fetchpred()
-    return render_template('index.html',sarima_cin=sarima_cin)
+    print(len(sarima_cin))
+    return render_template('index.html',sarima_cin=json.dumps(sarima_cin),pyhat_cin=json.dumps(pyhat_cin), pyl_cin=pyl_cin, pyu_cin=pyu_cin)
 
 
 if __name__ == '__main__':
