4da3a9f1b3b3bff387059db39ce2200009a42fe8
bhargavyagnik <41851612+bhargavyagnik@users.noreply.github.com>
2020-06-15 01:40:09 +0530

    updated

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ def home():
     sarima_gfemale, pyhat_gfemale, pyl_gfemale, pyu_gfemale = csvdata.fetchpred('gfemale')
     sarima_com_ab, pyhat_com_ab, pyl_com_ab, pyu_com_ab = csvdata.fetchpredcom('com_ab')
     sarima_com_pr, pyhat_com_pr, pyl_com_pr, pyu_com_pr = csvdata.fetchpredcom('com_pr')
+    actual_cin=[189325,197496,206405,215709,225560,235447,245418,255401,265388]
+    actual_cmh=[66012,68373,70660,73220,76153,78579,81318,84325,86878]
+    actual_din=[5229,5305,5408,5530,5653,5792,5912,6003,6112]
+    actual_dmh=[2276,2352,2455,2577,2700,2839,2959,3050,3159]
+    actual_gmale=[40682,42211,43679,45084,46652,48444,49943,51590,53443]
+    actual_gfemale=[24448,25430,26294,27176,28168,29309,30246,31338,32492]
     return render_template('index.html',sarima_cin=json.dumps(sarima_cin),pyhat_cin=json.dumps(pyhat_cin),
                            pyl_cin=json.dumps(pyl_cin), pyu_cin=json.dumps(pyu_cin),sarima_cmh=json.dumps(sarima_cmh),
                            pyhat_cmh=json.dumps(pyhat_cmh),pyl_cmh=json.dumps(pyl_cmh), pyu_cmh=json.dumps(pyu_cmh),
@@ -29,7 +35,9 @@ def home():
                            pyhat_com_ab=json.dumps(pyhat_com_ab),pyl_com_ab=json.dumps(pyl_com_ab),
                            pyu_com_ab=json.dumps(pyu_com_ab),sarima_com_pr=json.dumps(sarima_com_pr),
                            pyhat_com_pr=json.dumps(pyhat_com_pr),pyl_com_pr=json.dumps(pyl_com_pr),
-                           pyu_com_pr=json.dumps(pyu_com_pr))
+                           pyu_com_pr=json.dumps(pyu_com_pr),actual_cin=json.dumps(actual_cin),
+                           actual_cmh=json.dumps(actual_cmh),actual_din=json.dumps(actual_din),actual_dmh=json.dumps(actual_dmh),
+                           actual_gmale=json.dumps(actual_gmale),actual_gfemale=json.dumps(actual_gfemale))
 
 
 if __name__ == '__main__':
